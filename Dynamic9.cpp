6668c33031b47aae598cf18ce50c6ab1456956ed
Jesus0342 <jsanchez97@saddleback.edu>
2017-02-17 10:39:13 -0800

    finished commenting

--- Dynamic9.cpp
+++ Dynamic9.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 
 DynArray::DynArray(/* IN */ int arrSize)
-		  // Constructor.
-		  // PRE:  arrSize is assigned
-		  // POST: IF arrSize >= 1 && enough memory THEN
-		  // 	   Array of size arrSize is created with
-		  //       all elements == 0  ELSE error message.
+// Constructor.
+// PRE:  arrSize is assigned
+// POST:  IF arrSize >= 1 && enough memory
+//        THEN Array of size arrSize is created with all elements == 0
+//        ELSE error message.
 {
 	int i;
 
-	if (arrSize < 1)
+	if(arrSize < 1)
 	{
 		std::cerr << "DynArray constructor - invalid size: "
 				  << arrSize << std::endl;
@@ -22,92 +22,91 @@ DynArray::DynArray(/* IN */ int arrSize)
 
 	size = arrSize;
 
-	for (i = 0; i < size; i++)
+	for(i = 0; i < size; i++)
 	{
 		arr[i] = 0;
 	}
 }
 
+DynArray::DynArray(const DynArray& otherArr)
+// Copy constructor
+// Implicitly called for deep copy in initializations.
+// POST:  IF room on free store
+//		  THEN new array of size otherArr.size is created on free store
+//        && arr == its base address
+//        && size == otherArr.size
+// 	      && arr[0..size-1] == otherArr.arr[0..size-1]
+// 	      ELSE error message.
+{
+	int i;
+
+	size = otherArr.size;
+	arr  = new int[size];	      // allocate memory for copy
+
+	for(i = 0; i< size ; i++)
+	{
+		arr[i] = otherArr.arr[i]; // copies array
+	}
+}
+
+DynArray::~DynArray()
+// Destructor.
+// POST: Memory for dynamic array deallocated.
+{
+	delete [] arr;
+}
+
+int DynArray::ValueAt (/* IN */ int i)const
+// PRE:  i is assigned.
+// POST: IF 0 <= i < size
+//       THEN FCTVAL == arr[i]
+//       ELSE halt with error message.
+{
+	if(i < 0 || i >= size)
+	{
+		std::cerr << "ValueAt - invalid index : " << i << std::endl;
+//		exit(1);
+	}
+
+	return  arr[i];
+}
+
 void DynArray::Store(/* IN */ int val, /* IN */ int i )
-	 // PRE:  val and i are assigned
-	 // POST: IF 0 <= i < size of this array THEN
-	 // 	  arr[i] == val
-	 // 	  ELSE error message.
+ // PRE:  val and i are assigned
+ // POST: IF 0 <= i < size of this array
+ // 	  THEN arr[i] == val
+ // 	  ELSE error message.
  {
 
-	if (i < 0 || i >= size)
+	if(i < 0 || i >= size)
 	{
 		std::cerr << "Store - invalid index : " << i << std::endl;
-		exit(1);
+//		exit(1);
 	}
 
 	arr[i] = val;
 
 }
 
-int DynArray::ValueAt (/* IN */ int i)const
-	// PRE:  i is assigned.
-	// POST: IF 0 <= i < size THEN
-	// FCTVAL == arr[i]
-	// ELSE halt with error message.
-{
-	if (i < 0 || i >= size)
-	{
-		std::cout << "ValueAt - invalid index : " << i << std::endl;
-		exit(1);
-	}
-
-	return  arr[i];
-}
-
-DynArray::~DynArray()
-		// Destructor.
-		// POST: Memory for dynamic array deallocated.
-{
-	delete [] arr;
-}
-
-DynArray::DynArray(const DynArray& otherArr)
-		// Copy constructor
-		// Implicitly called for deep copy in initializations.
-		// POST:  If room on free store THEN
-		// 	new array of size otherArr.size is created
-		// 	on free store && arr == its base address
-		//     && size == otherArr.size
-		 // 	&& arr[0..size-1] == otherArr.arr[0..size-1]
-		// 	ELSE error message.
+void DynArray::CopyFrom(/* IN */ DynArray  otherArr)
+// Creates a deep copy of otherArr.
+// POST:  Array pointed to by arr@entry deallocated &&
+// 		  IF room on free store THEN new array is created on free store
+//	      && arr == its base address
+//	      && size == otherArr.size
+//	      && arr[0..size-1] == otherArr[0..size-1]
+//        ELSE halts with error message.
 {
 	int i;
 
+	delete []  arr;			      // delete current array
+
 	size = otherArr.size;
-	arr  = new int[size];	  // allocate memory for copy
+	arr  = new int [size];		  // allocate new array
 
-	for (i = 0; i< size ; i++)
+	for(i = 0; i < size; i++)     // deep copy array
 	{
-		arr[i] = otherArr.arr[i];        // copies array
-	}
-}
-
-void DynArray::CopyFrom(/* IN */  DynArray  otherArr )
-		// Creates a deep copy of otherArr.
-		// POST:  Array pointed to by arr@entry deallocated
-		//   &&  IF room on free store
-		//	  THEN new array is created on free store
-		//	      && arr == its base address
-		//	      && size == otherArr.size
-		//	      && arr[0..size-1] == otherArr[0..size-1]
-		//        ELSE halts with error message.
-{
-	int i ;
-
-	delete []  arr;			// delete current array
-
-	size = otherArr.size ;
-	arr  = new int [size] ;		// allocate new array
-
-	for(i = 0; i < size ; i++)    // deep copy array
-	{
-		arr[i] = otherArr.arr[i] ;
+		arr[i] = otherArr.arr[i];
 	}
 
 	std::cout << "copyfrom function is called" << std::endl << std::endl;
--- main.cpp
+++ main.cpp
@@ -1,35 +1,73 @@
+/******************************************************************************
+* AUTHOR          : Jesus Sanchez
+* STUDENT ID      : 1024834
+* EXTRA CREDIT #1 : Dynamic 9
+* CLASS           : CS1C
+* SECTION         : MW: 1:30pm
+* DUE DATE        : 02/22/2017
+* ****************************************************************************/
+
 #include "Dynamic9.h"
 #include <iostream>
 using namespace std;
 
+/******************************************************************************
+ * DYNAMIC 9
+ * ----------------------------------------------------------------------------
+ * This program uses a DynArray class to great dynamic arrays with a predefined
+ * size. Two objects of the class are instantiated and the dynamic array of the
+ * first object is initialized using the Store function and the elements are
+ * printed using the ValueAt function. Then, the array of the second object
+ * is initialized using the CopyFrom function and the elements are printed.
+ * Finally, to test the boundary check of the Store and ValueAt functions, an
+ * invalid index is passed into both.
+ * ----------------------------------------------------------------------------
+ * INPUT:
+ * 	  There is no input.
+ *
+ * OUTPUT:
+ *    array1 - Object holding a pointer to a dynamic array and its size.
+ *    array2 - Object holding a pointer to a dynamic array and its size - copied
+ *    		   from array1 using deep copy function.
+ *****************************************************************************/
 int main()
 {
-	DynArray array1(10);
-	DynArray array2(10);
+	const int SIZE = 10;
 
-	for(int i = 0; i < 10; i++)
+	DynArray array1(SIZE); // Dynamic Array object.
+	DynArray array2(SIZE); // Dynamic Array object.
+
+	// Initializes the elements of the first dynamic array by passing in the
+	// current index as the index and value to be stored.
+	for(int i = 0; i < SIZE; i++)
 	{
 		array1.Store(i, i);
 	}
 
-	for(int i = 0; i < 10; i++)
+	// Prints the contents of the dynamic array.
+	for(int i = 0; i < SIZE; i++)
 	{
-		cout << array1.ValueAt(i) << " " << array1.ValueAt(i) * 10 << endl;
+		cout << array1.ValueAt(i) << " " << array1.ValueAt(i) * SIZE << endl;
 	}
 
 	cout << endl;
 
+	// Calls the deep copy function to copy the elements and size of the first
+	// dynamic array into the elements of the second dynamic array.
 	array2.CopyFrom(array1);
 
-	for(int i = 0; i < 10; i++)
+	// Prints the contents of the second dynamic array.
+	for(int i = 0; i < SIZE; i++)
 	{
 		cout << array2.ValueAt(i) << " " << array2.ValueAt(i) * 10 << endl;
 	}
 
 	cout << endl;
 
+	// Passes an invalid index into the Store and ValueAt functions to test
+	// the boundary checking statements.
 	array2.Store(11, 11);
-	cout << array2.ValueAt(11);
+	array2.ValueAt(11);
 
 	return 0;
 }
